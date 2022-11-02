fbf1c1519e78c8af685d6588a497344df4348a53
druckmax <max_sommerfeld@web.de>
2022-11-02 22:05:43 +0100

    Replaces avatar image with more colorful one

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./_Hero.scss";
 import { useContext } from "react";
 
-import test_avatar from "./img/avatar-white-edit.png";
+// import test_avatar from "./img/avatar-white-edit.png";
+import test_avatar from "./img/avatar_bg_edit_01_transparent_small_0.5.png";
 import { MainContext } from "../../Context";
 
 function Hero() {
-  const {heroScrollHandler, contactRef} = useContext(MainContext);
+  const { heroScrollHandler, contactRef } = useContext(MainContext);
 
   return (
     <div className="hero-container">
@@ -14,7 +15,12 @@ function Hero() {
         <h1>Hey I'm Max...</h1>
         <div className="sub-heading">
           <p>I'm a Junior Web Developer and Designer based in Leipzig.</p>
-          <button onClick={() => heroScrollHandler(contactRef)} className="btn hero-btn">Contact me</button>
+          <button
+            onClick={() => heroScrollHandler(contactRef)}
+            className="btn hero-btn"
+          >
+            Contact me
+          </button>
         </div>
       </div>
 
